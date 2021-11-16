5dc676b24c25df5e7b02b39cb685f432219ad23e
Cyber-Tapok <slava1152@gmail.com>
2021-11-17 00:16:08 +0400

    add decode

--- src/main/kotlin/lab5/Main.kt
+++ src/main/kotlin/lab5/Main.kt
@@ -1,12 +1,23 @@
 package lab5
 
+import matrix.multiply
+import matrix.utils.out
+import matrix.utils.toMatrix
+import matrix.utils.toRow
+
 fun main() {
 
     val m = 4
-    val r = 3
+    val r = 2
 
-    val generating = g(r, m)
+    val generating = g(r, m).map { it.first }.toMatrix()
     println(g(r, m).joinToString(separator = "\n"))
+    val word = listOf(0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0)
+    val u = majorDecode(r, m, word).toRow()
+    word.toRow().out("w")
+    u.out("u")
+    u.multiply(generating).out("v")
+
 }
 
 
--- src/main/kotlin/lab5/VectorFuns.kt
+++ src/main/kotlin/lab5/VectorFuns.kt
@@ -1,6 +1,8 @@
 package lab5
 
 import matrix.allWordsForLength
+import matrix.utils.Row
+import matrix.utils.xorPlus
 import kotlin.math.pow
 
 fun v(I: List<Int>, x: List<Int>) = if (I.all { x[it] == 0 }) 1 else 0
@@ -38,4 +40,58 @@ fun vectorICombination(r: Int, m: Int): List<List<List<Int>>> {
         result[list.size] += list
     }
     return result
+}
+
+fun generateBitmask(r: Int, m: Int): List<List<List<Int>>> {
+    val result = List(r + 1) { mutableListOf<List<Int>>() }
+    (0 until 2.0.pow(m).toInt()).forEach huy@{ num ->
+
+        val list = num.toBinaryList()
+            .toMutableList()
+
+        while (list.size < m) {
+            list.add(0, 0)
+        }
+        val realSize = list.filter { it != 0 }.size
+        if (realSize > r) return@huy
+        result[realSize] += list
+    }
+    return result
+}
+
+fun majorDecode(r: Int, m: Int, word: List<Int>): List<Int> {
+    val words = allWordsForLength(m, true).map { it.reversed() }
+    val masks = generateBitmask(r, m)
+    var len = 0
+    val res = mutableListOf<List<Int>>()
+    var wordCopy = word
+    (r downTo 0).forEach { i ->
+        val arr = mutableListOf<List<Int>>()
+        masks[i].forEach { mask ->
+            len++
+            val I = mask.mapIndexedNotNull { index, i -> if (i == 1) index else null }
+            val reversedMask = mask.map { it xor 1 }
+            val IC = reversedMask.mapIndexedNotNull { index, i -> if (i == 1) index else null }
+            val H = words.filter { v(I, it) == 1 }
+            val v_t = H.map { h -> words.map { w -> v(IC, w xorPlus h) } }
+            val wWithT = v_t.map { v ->
+                v.foldIndexed(0) { index, acc, i ->
+                    (acc + i * wordCopy[index]) % 2
+                }
+            }
+            val decision = (wWithT.sum() * 2) > wWithT.size
+            if (decision) {
+                arr += words.map { x -> v(I, x) }
+                res += I
+            }
+        }
+        arr.forEach { x -> wordCopy = wordCopy xorPlus x }
+    }
+    val answer = List(len) { 0 }.toMutableList()
+    val IOrder = g(r, m).map { it.second }
+    res.forEach { x ->
+        val index = IOrder.indexOf(x)
+        answer[index] = 1
+    }
+    return answer
 }
